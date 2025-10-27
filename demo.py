7171657cceadeb682eb815e005e21673f57d7cb3
zhy <648423370@qq.com>
2025-10-27 20:49:05 +0900

    Add the weight files and specify the weight in `demo.py`.

--- demo.py
+++ demo.py
@@ -25,7 +25,7 @@ from collision_detector import ModelFreeCollisionDetector
 from data_utils import CameraInfo, create_point_cloud_from_depth_image
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--checkpoint_path', required=True, help='Model checkpoint path')
+parser.add_argument('--checkpoint_path', default='./weights/checkpoint-kn.tar', help='Model checkpoint path')
 parser.add_argument('--num_point', type=int, default=20000, help='Point Number [default: 20000]')
 parser.add_argument('--num_view', type=int, default=300, help='View Number [default: 300]')
 parser.add_argument('--collision_thresh', type=float, default=0.01, help='Collision Threshold in collision detection [default: 0.01]')
